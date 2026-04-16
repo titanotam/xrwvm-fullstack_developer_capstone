a0e13cf5ca478bd004bcb847260f5d47b7ac5ec3
huynhnhuttam <huynhnhuttam@gmail.com>
2026-04-15 22:12:05 -0400

    Update app

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -18,5 +18,7 @@ urlpatterns = [
     # path for dealer reviews view
 
     # path for add a review view
+    
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
